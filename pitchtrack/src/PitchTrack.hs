2f57f5ad655dc637609066b7590e16baec8cd009
Lorenzo <lortabac@gmx.com>
2015-10-07 20:04:09 +0200

    Re-exported the PitchTrack.Pipes module

--- pitchtrack/src/PitchTrack.hs
+++ pitchtrack/src/PitchTrack.hs
@@ -1,11 +1,14 @@
 module PitchTrack (
+-- * Base type
     PitchTrack
   , runPitchTrack
 -- * Re-exported modules
   , module PitchTrack.Track
+  , module PitchTrack.Pipes
 -- * Utilities
   , neededSampleNum
   ) where
 
 import           DywaPitchTrack
+import           PitchTrack.Pipes
 import           PitchTrack.Track
